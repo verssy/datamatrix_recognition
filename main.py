4a7c3098e2d07e1770f59454be0e305e7f9f7129
Дубровин Андрей Николаевич <@domain.local>
2022-07-04 15:34:11 +0700

    add easyocr for text recognition

--- main.py
+++ main.py
@@ -1,95 +1,121 @@
 
+import re
 import os
-import cv2 as cv
-from cv2 import dnn_DetectionModel
-import numpy as np
-import time
+import cv2
+import pytesseract
+import pylibdmtx.pylibdmtx as pdmtx
+import easyocr
 
+pytesseract.pytesseract.tesseract_cmd=r'C:\Users\Дубровин\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
-class Detector:
-    def __init__(self, video_path, config_path, model_path, classes_path):
-        self.video_path = video_path
-        self.config_path = config_path
-        self.model_path = model_path
-        self.classes_path = classes_path
+# args
+files_prefix = "./imgs/barcode_"
+max_sides_ratio = 1.5
+min_area = 1000
+delta_xy = 10
+bool_save_images = False
+bool_video = True
+bool_fast_decode = False
 
-        self.net = cv.dnn_DetectionModel(self.model_path, self.config_path)
-        self.net.setInputSize(320, 320)
-        self.net.setInputScale(1.0/127.5)
-        self.net.setInputMean((127.5, 127.5, 127.5))
-        self.net.setInputSwapRB(True)
+if bool_save_images:
+    imgs_exists = os.path.exists("imgs")
+    if not imgs_exists:
+        os.makedirs("imgs")
 
-        self.readClasses()
+text_reader = easyocr.Reader(['ru'], gpu=True)
+files_count, images = 0, []
 
-    def readClasses(self):
-        with open(self.classes_path, 'r') as f:
-            self.classes_list = f.read().splitlines()
-        self.classes_list.insert(0, '__Background__')
-        self.color_list = np.random.uniform(low=0, high=255, size=(
-            len(self.classes_list), 3))
-        print(self.classes_list)
+if bool_video: cap = cv2.VideoCapture('./vid_2.mp4')
 
-    def onVideo(self):
-        cap = cv.VideoCapture(self.video_path)
-        if (cap.isOpened() == False):
-            print('Fuck yourself, you cunt')
-            return
-        success, frame = cap.read()
-        timeEnd = 0
-        timeStart = 0
+while True:
+    if bool_video:
+        _, frame = cap.read()
+        if not _: break
+    else: frame = cv2.imread('test1.jpg')
 
-        while success:
-            classLabelIds, confidences, bboxs = self.net.detect(
-                frame, confThreshold=0.5)
-            bboxs = list(bboxs)
-            confidences = list(np.array(confidences).reshape(1, -1)[0])
-            confidences = list(map(float, confidences))
-            bboxIdx = cv.dnn.NMSBoxes(
-                bboxs, confidences, score_threshold=0.5, nms_threshold=0.2)
-            if len(bboxIdx) != 0:
-                for i in range(0, len(bboxIdx)):
-                    bbox = bboxs[np.squeeze(bboxIdx[i])]
-                    classConfidence = confidences[np.squeeze(bboxIdx[i])]
-                    classLabelId = np.squeeze(
-                        classLabelIds[np.squeeze(bboxIdx[i])])
-                    classLabel = self.classes_list[classLabelId]
-                    classColor = [int(c)
-                                  for c in self.color_list[classLabelId]]
-                    classColor = (0, 0, 255)
+    # text recognition
+    recognized = text_reader.readtext(frame, paragraph=True)
+    for (bbox, text, prob) in recognized:
+        (tl, tr, br, bl) = bbox
+        tl = (int(tl[0]), int(tl[1]))
+        tr = (int(tr[0]), int(tr[1]))
+        br = (int(br[0]), int(br[1]))
+        bl = (int(bl[0]), int(bl[1]))
+        cv2.rectangle(frame, tl, br, (0, 255, 0), 1)
+        cv2.putText(frame, text, (tl[0], tl[1] - 10), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 0), 2)
+    
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.resize(img_gray, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+    img_gray = cv2.GaussianBlur(img_gray, (3, 3), 0)
+    harris = cv2.cornerHarris(img_gray, 4, 3, 0.06)
+    __, thr = cv2.threshold(harris, 0.001 * harris.max(), 255, cv2.THRESH_BINARY)
+    thr = thr.astype('uint8')
 
-                    displayText = "{}:{:.4f}".format(
-                        classLabel, classConfidence)
-                    x, y, w, h = bbox
+    #d = pytesseract.image_to_data(frame, output_type=pytesseract.Output.DICT)
+    #n_boxes = len(d['text'])
+    #for i in range(n_boxes):
+    #    if float(d['conf'][i]) > 60:
+    #        x, y, w, h = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+    #        text = d['text'][i]
+    #        cv2.rectangle(frame, (x, y), (x + w , y + h), (0, 255, 255), 2)
+    #        cv2.putText(frame, text, (x, y + h + 20), cv2.FONT_HERSHEY_DUPLEX,
+    #                1, (0, 255, 255), 2)
 
-                    cv.rectangle(frame, (x, y), (x + w, y + h),
-                                 color=classColor, thickness=1)
-                    cv.putText(frame, displayText, (x, y - 10),
-                               cv.FONT_HERSHEY_PLAIN, 1, classColor)
+    contours, _ = cv2.findContours(thr, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    contours = list(filter(lambda x: cv2.contourArea(cv2.convexHull(x)) > min_area, contours))
 
-                    if classLabel == "cell phone":
-                        cv.rectangle(frame, (x, y), (x + w, y + h),
-                                     color=(0, 255, 0), thickness=1)
-                        cv.putText(frame, displayText, (x, y - 10),
-                                   cv.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
+    if bool_save_images:
+        for con in contours:
+            (x, y, w, h) = cv2.boundingRect(con)
+            dx = min(x, delta_xy)
+            dy = min(y, delta_xy)
+            x = x - dx
+            y = y - dy
+            h = h + dy * 2
+            w = w + dx * 2
+            if not (w / h > max_sides_ratio or h / w > max_sides_ratio):
+                files_count = files_count + 1
+                tempimage = frame[y * 2: y * 2 + h * 2, x * 2: x * 2 + w * 2]
+                out_filename = str(files_prefix + str(files_count)) + ".jpeg"
+                cv2.imwrite(out_filename, tempimage)
+    
+    for con in contours:
+        (x, y, w, h) = cv2.boundingRect(con)
+        dx = min(x, 5)
+        dy = min(y, 5)
+        x = x - dx
+        y = y - dy
+        h = h + dy * 2
+        w = w + dx * 2
+        if not (w / h > max_sides_ratio or h / w > max_sides_ratio):
+            tempimage = frame[y * 2: y * 2 + h * 2, x * 2: x * 2 + w * 2]
+            if bool_fast_decode: data = pdmtx.decode(tempimage, shrink=2, max_count=1)
+            else: data = pdmtx.decode(tempimage)
+            text = ""
+            if len(data) > 0:
+                text = data[0].data.decode('UTF-8')
+                text = text.replace('\x1d', '\\x1d')
+                print(f"found barcode: {text}")
+                cv2.putText(frame, text, (x * 2, y * 2 + h * 2 + 20), cv2.FONT_HERSHEY_DUPLEX,
+                    1, (0, 0, 255), 2)
+                cv2.rectangle(frame, (x * 2, y * 2), (x * 2 + w * 2, y * 2 + h * 2), (0, 0, 255))
+            
+    cv2.imshow("image", frame)
+    if bool_video: images.append(frame)
 
-            cv.imshow("winname", frame)
-            key = cv.waitKey(1) & 0xFF
-            if key == ord('q'):
-                break
-            success, frame = cap.read()
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        print('I quit!')
+        break
 
-        cv.destroyAllWindows()
+print('complite!')
 
+if bool_video:
+    if len(images)>0:
+        height, width, layers = images[1].shape
+        video = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*'DIVX'), 20, (width, height))
+        for i in range(len(images)):
+            video.write(images[i])
+        video.release()
+    cap.release()
 
-def main():
-    video_path = 0
-    config_path = "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
-    model_path = "frozen_inference_graph.pb"
-    classes_path = "coco.names"
-
-    detector = Detector(video_path, config_path, model_path, classes_path)
-    detector.onVideo()
-
-
-if __name__ == '__main__':
-    main()
+cv2.destroyAllWindows()
